3dce0edb4f84bbb1aa4802e35b002efdaab78d31
Matthew Pagan <mastapegs01@gmail.com>
2026-01-16 09:38:38 -0500

    add ci for basic module deployment, 3 member cluster in ECS behind ALB

---
+++ ci/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+variable "container_image" {
+  type = string
+}
+
+variable "license_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "license_server_uri" {
+  type = string
+}
+
+module "quine_enterprise" {
+  source = "../"
+
+  project_name    = "quine-ci-test"
+  container_image = var.container_image
+  desired_count   = 3
+
+  java_opts = join(" ", [
+    "-Dquine.license-key=${var.license_key}",
+    "-Dquine.license-server-uri=${var.license_server_uri}",
+  ])
+}
